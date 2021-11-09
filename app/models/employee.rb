dc5569ec62e83235ffefd83d6bba250c3c90bfd3
Mrugendra Shilvant <sanketshilvant@gmail.com>
2021-11-09 15:34:12 +0530

    validations added

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,11 +1,16 @@
 class Employee < ApplicationRecord
+    before_save {self.email = email.downcase}
     has_one :purse
     has_one :payment_history, through: :purse
     
-    validates :FirstName, :LastName, :Address, :City, :State, :PostalCode, :PhoneNumber, :salary, :Email,  presence: true
+    validates :FirstName, :LastName, :Address, :City, :State, :PostalCode, :PhoneNumber, :salary,  presence: true
     validates :FirstName, :LastName, length: { minimum: 2 }
     validates :salary, :PhoneNumber, :PostalCode, numericality: true
-    validates :PhoneNumber,:Email, uniqueness: true
+    validates :PhoneNumber, uniqueness: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
+    validates :email, presence: true, length: { maximum: 105 },            
+                        uniqueness: { case_sensitive: false },            
+                        format: { with: VALID_EMAIL_REGEX }
 
     before_save :merge_address
    
